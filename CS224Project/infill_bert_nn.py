11f93eb134f32696a18aebe7c35a37421e7e5d1a
Will Li <willeli2002@gmail.com>
2023-03-19 00:28:53 -0700

    attempt 1 fine_tune

--- CS224Project/infill_bert_nn.py
+++ CS224Project/infill_bert_nn.py
@@ -8,7 +8,7 @@ import requests
 import os
 from transformers import AutoModelForSequenceClassification
 from transformers import TrainingArguments, Trainer
-from transformers import TFAutoModelForMaskedLM
+from transformers import TFAutoModelForMaskedLM, AutoModelForCausalLM
 from transformers import AutoTokenizer
 import evaluate
 # from nltk.corpus import stopwords
@@ -17,18 +17,15 @@ import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 
 TRAIN_VALID_SPLIT = 0.7
-BASE_MODEL_NAME = "distilbert-base-uncased"
+BASE_MODEL_NAME = 'bert-base-uncased'
+FT_MODEL_NAME = "distilbert-base-uncased"
 RANK_CUTTOFF = 25
 NUM_MASK = 3
 
-BASE_MODEL = TFAutoModelForMaskedLM.from_pretrained(BASE_MODEL_NAME)
-TOKENIZER = AutoTokenizer.from_pretrained(BASE_MODEL_NAME)
-
-class FinetunedBartTrainer(Trainer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    def compute_loss(self, model, inputs):
-       return 0
+BASE_MODEL = AutoModelForCausalLM.from_pretrained(BASE_MODEL_NAME)
+FT_MODEL = TFAutoModelForMaskedLM.from_pretrained(FT_MODEL_NAME)
+BASE_TOKENIZER = AutoTokenizer.from_pretrained(BASE_MODEL_NAME)
+FT_TOKENIZER = AutoTokenizer.from_pretrained(FT_MODEL_NAME)
 
 def get_preprocessed_data ():
   all_train_sents = []
@@ -40,6 +37,7 @@ def get_preprocessed_data ():
   np.random.seed(123)
   np.random.shuffle(all_train_sents)
   split_idx = int(TRAIN_VALID_SPLIT * len(all_train_sents))
+  split_idx = 5
   training_data = all_train_sents[:split_idx]
   validation_data = all_train_sents[split_idx:]
   return training_data, validation_data
@@ -82,34 +80,43 @@ def sentence_coherence_score_single(input_sentence):
     res = response.choices[0]['text'].strip()
     return float(res)
 
-def get_prob(model, encoded_sentence):    
+def get_prob(model, encoded_sentence, tf= False):    
     isGPT = BASE_MODEL_NAME[:4] == 'gpt2'
-    
     def get_word_prob(ids_so_far, true_token):
         with torch.inference_mode():
             if len(ids_so_far.size()) == 1 and not(isGPT):
-                ids_so_far = np.array(torch.unsqueeze(ids_so_far,0))
+                ids_so_far = torch.unsqueeze(ids_so_far,0)
             end_model = model(input_ids = ids_so_far)
             logits = end_model.logits
-            all_probs = tf.nn.softmax(logits, axis = -1)
-
+            all_probs = torch.nn.functional.softmax(logits, dim = -1)
             if isGPT: probability = all_probs[-1][true_token]
-            else: probability =  tf.reshape(all_probs, [-1])[true_token.item()]
-                
+            else: probability =  all_probs[0][-1][true_token]  
             return probability
+        
+    def tf_get_word_prob(ids_so_far, true_token):
+      if len(ids_so_far) == 1 and not(isGPT):
+          ids_so_far = np.expand_dims(ids_so_far,0)
+      end_model = FT_MODEL(input_ids = ids_so_far)
+      logits = end_model.logits
+      all_probs = tf.nn.softmax(logits, axis = -1)
+
+      if isGPT: probability = all_probs[-1][true_token]
+      else: probability =  tf.reshape(all_probs, [-1])[true_token.item()]
+          
+      return probability
     
     all_probs = np.zeros(len(encoded_sentence))
     total_log_prob = 0
     for i in range(0,len(encoded_sentence)):
-        word_cond_prob = get_word_prob(encoded_sentence[:i+1], encoded_sentence[i])
+        word_cond_prob = get_word_prob(encoded_sentence[:i+1], encoded_sentence[i]) if not tf else tf_get_word_prob(encoded_sentence[:i+1], encoded_sentence[i])
         all_probs[i] = word_cond_prob.numpy()
         total_log_prob += np.log(word_cond_prob)
     
     return total_log_prob, all_probs
 
-# REMOVE STOP WORDS
-def compute_perplexity(full_sentence, encoded_sentence):
-    base_log_prob, base_each_prob = get_prob(BASE_MODEL, encoded_sentence)
+# REMOVE STOP WORDS + CLS THINGS
+def compute_perplexity(encoded_sentence, tf = False):
+    base_log_prob, base_each_prob = get_prob(BASE_MODEL, encoded_sentence, tf = tf)
     # print(base_log_prob)
     N = len(encoded_sentence)
     
@@ -122,9 +129,10 @@ def mask_ith_word (sentence, i):
   words[i] = "[MASK]"
   return " ".join(words)
 
-def get_mask_indicies (full_sentence, encoded_sentence):
-  sentence_perplexity, prob_each_index = compute_perplexity(full_sentence, encoded_sentence)
+def get_mask_indicies (encoded_sentence, tf = False):
+  sentence_perplexity, prob_each_index = compute_perplexity(encoded_sentence, tf = tf)
   indexes_by_prob = [[p,i] for i,p in enumerate(prob_each_index)]
+  indexes_by_prob = indexes_by_prob [1: len(indexes_by_prob)]
   indexes_by_prob = sorted(indexes_by_prob)
 
   return [tu[1] for tu in indexes_by_prob[:NUM_MASK]]
@@ -148,30 +156,86 @@ def get_most_similar_token (tokens, original_encoding, index):
         top_token = token
    return top_token
 
-def fine_tune ():
-   return 
+class FinetunedBartTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    def compute_loss(self, model, inputs):
+       cross_entropy_loss, logits = model(
+                input_ids=inputs["input_ids"],
+                labels=inputs["labels"],
+                attention_mask=inputs["attention_mask"]
+        )
+       
+       return 0
+class BERTDataset(Dataset):
+  def __init__(self, encodings, labels):
+      self.encodings = encodings
+      self.labels = labels
+  def __len__(self):
+      return len(self.labels)
+  def __getitem__(self, idx):
+      item = {key: tf.Tensor(val[idx]) for key, val in self.encodings.items()}
+      item['labels'] = tf.Tensor(self.labels[idx])
+      return item
+    
+def prep_data_set (training_data):
+  dataset_train =  FT_TOKENIZER (training_data, return_tensors="np")
+  dataset_train["labels"] = np.copy(dataset_train["input_ids"])
+
+  # mask lowest perplexity token
+  for i, encoding in enumerate(dataset_train["input_ids"]):
+    base_encoding = BASE_TOKENIZER (training_data[i], return_tensors = 'pt')['input_ids']
+    mask_idx = get_mask_indicies (base_encoding[0])[0]
+    encoding[mask_idx] = FT_TOKENIZER.mask_token_id
+  return BERTDataset(dataset_train["input_ids"], dataset_train["labels"])
+
+def fine_tune (model, training_data):
+  inputs = prep_data_set (training_data)
+  print (inputs)
+   
+  # training_args = TrainingArguments(
+  #       output_dir="test_trainer", 
+  #       evaluation_strategy="epoch",
+  #       num_train_epochs = 3,
+  #       gradient_accumulation_steps = 1,
+  #       per_device_train_batch_size = 8,
+  #       learning_rate = 5e-5,
+  #       logging_steps = 400
+  #   )
+   
+  # trainer = FinetunedBartTrainer(
+  #   model=model,
+  #   args=training_args,
+  #   train_dataset=inputs
+  #  )
+  # trainer.train()
   
 
 if __name__ == "__main__":
   print ("\n\n\n BEGINGING RUN\n")
   training_data, validation_data  = get_preprocessed_data ()
 
-  sentence = validation_data[0]
-  print (sentence)
-  encoding = TOKENIZER (sentence, return_tensors = 'pt')['input_ids']
-  mask_indicies = get_mask_indicies (sentence, encoding[0])
-  inputs = TOKENIZER (sentence, return_tensors="np")
-  for i in range (NUM_MASK):
-    inputs['input_ids'][0][ mask_indicies[i]] = TOKENIZER.mask_token_id
-    token_logits = BASE_MODEL(**inputs).logits
-    mask_token_index = mask_indicies[i]
-    mask_token_logits = token_logits[0, mask_token_index, :]
-    top_tokens = np.argsort(-mask_token_logits)[:RANK_CUTTOFF].tolist()
-    top_token = get_most_similar_token (top_tokens, inputs['input_ids'][0], mask_indicies[i])
-    inputs['input_ids'][0][ mask_indicies[i]] = top_token
-    new_sentence = TOKENIZER.decode(inputs['input_ids'][0])
-    print (new_sentence)
+  # BEGIN FINE TUNING
+  fine_tune (FT_MODEL, training_data)
 
+  old_new_dict = {}
+
+  for i, sentence in enumerate(validation_data):
+    print (sentence)
+    encoding = BASE_TOKENIZER (sentence, return_tensors = 'pt')['input_ids']
+    mask_indicies = get_mask_indicies (encoding[0])
+    inputs = FT_TOKENIZER (sentence, return_tensors="np")
+    print (inputs)
+    for i in range (NUM_MASK):
+      inputs['input_ids'][0][mask_indicies[i]] = FT_TOKENIZER.mask_token_id
+      token_logits = FT_MODEL(**inputs).logits
+      mask_token_index = mask_indicies[i]
+      mask_token_logits = token_logits[0, mask_token_index, :]
+      top_tokens = np.argsort(-mask_token_logits)[:RANK_CUTTOFF].tolist()
+      top_token = get_most_similar_token (top_tokens, inputs['input_ids'][0], mask_indicies[i])
+      inputs['input_ids'][0][ mask_indicies[i]] = top_token
+      new_sentence = FT_TOKENIZER.decode(inputs['input_ids'][0])
+      print (new_sentence)
 
 
 
